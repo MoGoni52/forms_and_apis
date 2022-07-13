6997a515ad40a967675b26e86606bae38e480ace
mohammedgoni <mohammedsigoni@gmail.com>
2022-07-13 15:26:12 +0100

    Working form example

---
+++ assets/scripts/form.js
@@ -0,0 +1,31 @@
+function addListItem(text) {
+    
+    // Get a reference to the container
+    const container = document.getElementById("container");
+
+    // Create a li element
+    const newElement = document.createElement("li");
+
+    // Add the text to it
+    newElement.textContent = text;
+
+    // Actually addd the element to the page
+    container.appendChild(newElement);
+
+}
+
+
+function handleFormSubmit (e) {
+
+    // Stop the page refreshing
+    e.preventDefault();
+
+    // Get reference to the form
+    const form = e.target;
+
+    // Add the name to the page
+    addListItem(form.name.value);
+
+}
+
+document.getElementById("identity-form").addEventListener("submit", handleFormSubmit)
